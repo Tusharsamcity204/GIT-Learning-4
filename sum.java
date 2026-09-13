3ca8af225ab08cfb53950ae1b73d3d2c13df7f3d
Aman Kumar <amansamastipur123@gmail.com>
2026-09-13 09:33:33 +0530

    adding the sum.java file in master branch directly

---
+++ sum.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many numbers do you want to add? ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print("Enter number " + i + ": ");
+            int number = sc.nextInt();
+
+            sum = sum + number;
+        }
+
+        System.out.println("Sum = " + sum);
+
+        sc.close();
+    }
+}
