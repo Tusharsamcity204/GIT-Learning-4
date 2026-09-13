52b35fa87165e0db6b1926638d29762f4dae7299
Aman Kumar <amansamastipur123@gmail.com>
2026-09-13 10:02:55 +0530

    adding sum.java

--- sum.java
+++ sum.java
@@ -17,7 +17,7 @@ public class Sum {
             sum = sum + number;
         }
 
-        System.out.println("Sum   = " + sum);
+        System.out.println("Sum of desired number is   = " + sum);
 
         sc.close();
     }
